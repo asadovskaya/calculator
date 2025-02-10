abc19a70c2cc1b3eaf76b3e60e908bf397162926
Anastasiya Sadovskaya <55981128+asadovskaya@users.noreply.github.com>
2025-02-10 17:30:18 +0100

    Create src/test_calculator.py

---
+++ src/test_calculator.py
@@ -0,0 +1,28 @@
+import unittest
+from calculator import Calculator
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(Calculator.add(2, 3), 5)
+        self.assertEqual(Calculator.add(-1, 1), 0)
+        self.assertEqual(Calculator.add(-1, -1), -2)
+
+    def test_subtract(self):
+        self.assertEqual(Calculator.subtract(10, 5), 5)
+        self.assertEqual(Calculator.subtract(-1, 1), -2)
+        self.assertEqual(Calculator.subtract(-1, -1), 0)
+
+    def test_multiply(self):
+        self.assertEqual(Calculator.multiply(3, 7), 21)
+        self.assertEqual(Calculator.multiply(-1, 1), -1)
+        self.assertEqual(Calculator.multiply(-1, -1), 1)
+
+    def test_divide(self):
+        self.assertEqual(Calculator.divide(10, 2), 5)
+        self.assertEqual(Calculator.divide(-1, 1), -1)
+        self.assertEqual(Calculator.divide(-1, -1), 1)
+        with self.assertRaises(ValueError):
+            Calculator.divide(1, 0)
+
+if __name__ == '__main__':
+    unittest.main()
